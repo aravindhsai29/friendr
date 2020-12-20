3903e2668868e9246b2aaafe0d648daf9829f226
Aravindh Ravichandran <aravindhravichandran@Aravindhs-MacBook-Pro.local>
2020-12-20 17:59:28 -0500

    Beta phase

---
+++ src/Friendrcards.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react';
+import "./Friendrcards.css";
+import TinderCard from "react-tinder-card";
+import { SwipeableDrawer } from '@material-ui/core';
+import axios from './axios';
+function Friendrcards() {
+    const [people, setPeople]  = useState([]);
+
+    useEffect(() => {
+        async function fetchData() 
+        {
+            const req = await axios.get("/tinder/cards");
+
+            setPeople(req.data);
+        }
+        fetchData();
+    }, []);
+
+    console.log(people);
+
+
+    const swiped = (direction, nameToDelete)=> {
+        console.log("removing:" +nameToDelete);
+        };
+
+    const outOfFrame = (name)=>
+    {
+        console.log(name + " left the screen!");
+    };
+    return (
+        <div className="friendrcards">
+            <div className = "friendrCards__cardContainer">
+
+
+            </div>
+         {people.map((person)=>(
+             <TinderCard
+             className="swipe"
+             key={person.name}
+             preventSwipe={["dp","down"]}
+             onSwipe={(dir)=> swiped(dir, person.name)}
+             onCardLeftScreen={()=> outOfFrame(person.name)}
+             >
+               <div
+               style = {{ backgroundImage: `url(${person.imgUrl})`  }}
+               className="card"
+               >
+                   <h3>{person.name}</h3>
+                   </div>  
+             </TinderCard> 
+         ))}   
+        </div>
+    )
+}
+
+export default Friendrcards
